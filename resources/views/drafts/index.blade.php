f88b188cff2d01b9d7d26e89c7a31135cabe00aa
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-08-23 10:59:34 +0100

    ok

--- resources/views/drafts/index.blade.php
+++ resources/views/drafts/index.blade.php
@@ -190,7 +190,7 @@
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                         </svg>
-                        Create Your First Article
+                        Create An Article
                     </a>
                 </div>
             @endforelse
