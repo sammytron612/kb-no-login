e339469b9e5236ed4f6d6bb5cac01ee76d35d18c
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-08-24 19:50:02 +0100

    ok

--- app/Mail/NewArticleMail.php
+++ app/Mail/NewArticleMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 
-class NewArticleMail extends Mailable implements ShouldQueue
+class NewArticleMail extends Mailable
 {
     use Queueable, SerializesModels;
 
